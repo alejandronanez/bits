6df7b018c74108b11f5c6181a9475c46d10fa2e3
Alejandro Nanez <alejonanez@gmail.com>
2018-03-09 15:18:31 -0500

    initial commit

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'prettier';
+
+export class Card extends Component {
+  render() {
+    return (
+      <View>
+        <Text>I am a card</Text>
+      </View>
+    );
+  }
+}
